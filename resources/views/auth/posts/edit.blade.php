778a8f3138fa4cfbc8080d879abdf4d0e5194bcd
Said Lagauit <lagaouit.said@gmail.com>
2023-11-06 19:18:13 +0000

    first commit

--- resources/views/auth/posts/edit.blade.php
+++ resources/views/auth/posts/edit.blade.php
@@ -1,41 +1,38 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Blog Theme | Edit')
+@section('title', 'Blog Theme | Edit : ' . $post->title)
 @section('meta_description', '')
 @section('keywords', '')
 
 @section('dashboard-content')
 
     <div class="edit-post">
+        <h1>Edit Post : {{ $post->title }}</h1>
         <form method="POST" action="{{ route('auth.posts.update', ['id' => $post->id]) }}" enctype="multipart/form-data" autocomplete="off">
             @csrf
             @method('PUT')
             <div class="row g-3">
                 <div class="col-md-4">
                     <img src="{{ asset('storage/' . $post->imgCover) }}" alt="{{ $post->title }}" class="w-100" title="{{ $post->title }}">
-                </div>
-                <div class="col-md-8">
                     <div class="form-group">
                         <label for="imgCover">Image Cover</label>
                         <input type="file" class="form-control" id="imgCover" name="imgCover" accept="image/*">
                     </div>
-
                     <div class="form-group">
                         <label for="title">Title</label>
                         <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
                     </div>
-
                     <div class="form-group mb-3">
                         <label for="meta_description">Description</label>
-                        <textarea id="meta_description" class="form-control" name="meta_description" rows="2">{{ $post->meta_description }}</textarea>
+                        <textarea id="meta_description" class="form-control" name="meta_description" rows="4">{{ $post->meta_description }}</textarea>
                         <p class="form-text text-muted"><span id="char-count">0</span>/160 characters</p>
                     </div>
-
                     <div class="form-group">
                         <label for="keywords">Keywords</label>
                         <input type="text" id="keywords" class="form-control" name="keywords" value="{{ $post->keywords }}">
                     </div>
-
+                </div>
+                <div class="col-md-8">
                     <div class="form-group">
                         <label for="content">Content</label>
                         <textarea class="form-control" id="content" name="content" rows="10">{{ $post->content }}</textarea>
@@ -43,7 +40,6 @@
                     <button type="submit" class="btn btn-primary">Update Post</button>
                 </div>
             </div>
-
         </form>
     </div>
 
